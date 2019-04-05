6593f74c09059956c92a403d7fb31df99d8b39de
Daniel Triebel <dtriebel@samson.de>
2019-04-05 09:11:39 +0200

    update login/register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { AuthGuard } from './auth/auth.guard';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './pages/user/user.component';
+import { RegistrationComponent } from './pages/user/registration/registration.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/user/login', pathMatch: 'full'},
+  {
+    path: 'user', component: UserComponent,
+    children: [
+      { path: 'registration', component: RegistrationComponent },
+      { path: 'login', component: LoginComponent }
+    ]
+  },
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]}
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/user/login/login.component';
 import { RezeptAnlegenComponent } from './pages/rezept-anlegen/rezept-anlegen.component';
 import { RezeptDetailseiteComponent } from './pages/rezept-detailseite/rezept-detailseite.component';
 import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { RegistrationComponent } from './pages/user/registration/registration.component';
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
@@ -18,13 +26,26 @@ import { HomeComponent } from './pages/home/home.component';
     LoginComponent,
     RezeptAnlegenComponent,
     RezeptDetailseiteComponent,
-    HomeComponent
+    HomeComponent,
+    UserComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      progressBar: true
+    }),
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
